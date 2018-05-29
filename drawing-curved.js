c7169f2c7bc1e4de08fbfc61744cb3d4f5bbb720
Sam O'Shaughnessy <samoshaughnessy123@hotmail.com>
2018-05-29 15:24:28 +0800

    curvy

--- drawing-curved.js
+++ drawing-curved.js
@@ -8,34 +8,50 @@ class DrawingCurved extends PaintFunction{
 
     onMouseDown(coord, event){
         if (this.actionCounter === 0){
-        this.contextReal.fillStyle = "#ffff00";
+        this.contextReal.lineCap = 'round';
+        this.contextDraft.lineCap = 'round';
         this.contextReal.strokeStyle = "black"; 
-        this.contextDraft.fillStyle = "#ffff00";
         this.contextDraft.strokeStyle = "black";  
         this.contextReal.lineWidth = 5;
         this.contextDraft.lineWidth = 5;
-        this.contextReal.beginPath(); 
         this.origX = coord[0];
         this.origY = coord[1];
+        this.contextReal.beginPath(); 
         this.contextReal.moveTo(this.origX, this.origY);
         console.log('yummy');
-    }else if (this.actionCounter === 1){
     }
-}
+    else if (this.actionCounter === 1){
+    }
+    }
 
     onDragging(coord,event){
         if (this.actionCounter === 0){
-        
+        this.endX = coord[0];
+        this.endY = coord[1];
+        this.contextDraft.closePath();
         this.contextDraft.clearRect(0,0,canvasDraft.width,canvasDraft.height);
-        this.contextDraft.beginPath();        
+        this.contextDraft.beginPath();
         this.contextDraft.moveTo(this.origX, this.origY);
-        this.contextDraft.quadraticCurveTo(cp[0], cp[1], coord[0], coord[1]); 
-        this.contextDraft.fill();
+        this.contextDraft.quadraticCurveTo(this.origX, this.origY, this.endX, this.endY); 
+        this.contextDraft.stroke();
+        } else if (this.actionCounter === 1){
+        this.contextDraft.clearRect(0,0,canvasDraft.width,canvasDraft.height);
+        this.contextDraft.beginPath();
+        this.contextDraft.moveTo(this.origX,this.origY);
+        this.contextDraft.quadraticCurveTo(coord[0],coord[1],this.endX,this.endY);
         this.contextDraft.stroke();
         }
+    }
 
-    onMouseUp()
-
-
+    onMouseUp(coord, event){
+        if (this.actionCounter === 0){
+            this.actionCounter = 1;
+        } else if (this.actionCounter === 1){
+            this.contextDraft.clearRect(0,0,canvasDraft.width,canvasDraft.height);
+            this.contextReal.quadraticCurveTo(coord[0],coord[1],this.endX,this.endY);
+            this.contextReal.stroke();
+            this.actionCounter = 0;
+    }
+    }
 
 }
