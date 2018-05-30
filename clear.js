ca6ab79ad49a7ddf870b14577315dff097fd5be6
Sam O'Shaughnessy <samoshaughnessy123@hotmail.com>
2018-05-30 10:32:56 +0800

    text fixed

--- clear.js
+++ clear.js
@@ -9,6 +9,6 @@ $('#clear-button').on("click", function () {
     contextReal.fillStyle = "white";
     contextReal.fillRect(0, 0, canvasReal.width, canvasReal.height);
     contextDraft.fillStyle = "white";
-    contextDraft.fillRect(0, 0, canvasReal.width, canvasReal.height);
+    contextDraft.fillRect(0, 0, canvasReal.width, canvasReal.height); 
 
 })
--- text.js
+++ text.js
@@ -1,50 +1,35 @@
-class Text extends PaintFunction{
-    constructor(contextReal,contextDraft){
+class Text extends PaintFunction {
+    constructor(contextReal, contextDraft) {
         super();
         this.contextReal = contextReal;
+        this.contextDraft = contextDraft;
+        this.fontWeight = 600;
+        this.fontSize = 15;
+        this.fontStyle = "Arial";
+        this.fillStyle = "black";
+        this.textX = [];
+        this.textY = [];
     }
 
-    onMouseDown(coord, event){  
-        addInput(event.clientX, event.clientY);
-        function addInput(x, y) {
-    
-            var input = document.createElement('input');
-            
-            input.type = 'text';
-            input.style.position = 'fixed';
-            input.style.left = (x - 4) + 'px';
-            input.style.top = (y - 4) + 'px';
-        
-            input.onkeydown = handleEnter;
-            
-            document.body.appendChild(input);
-        
-            input.focus();
-            
-            hasInput = true;
+    onMouseDown(coord, event) {
+        this.textX.push(coord[0]);
+        this.textY.push(coord[1]);
+        this.fontStartY = this.textY[0] - this.fontSize;
+        $('#textInput').css({ "display": "block", "transform": "translateY(" + coord[1] + "px) translateX(" + coord[0] + "px)", "font-size": this.fontSize + "px", "color": this.fillStyle, "font-family": this.fontStyle, "font-weight": this.fontWeight, "padding": "0", "border-style": "dotted", "opacity": "0.5" });
+        if ((this.textX.length > 1) && (event.target.id != $('#textInput'))) {
+            this.outputText(this.contextReal);
+            console.log('why no box?')
         }
-        
-        function handleEnter(e) {
-            var keyCode = e.keyCode;
-            if (keyCode === 13) {
-                drawText(this.value, parseInt(this.style.left, 10), parseInt(this.style.top, 10));
-                document.body.removeChild(this);
-                hasInput = false;
-            }
-        }
-        
-        function drawText(txt, x, y) {
-            this.contextReal.textBaseline = 'top';
-            this.contextReal.textAlign = 'left';
-            this.contextReal.font = font;
-            this.contextReal.fillText(txt, x - 4, y - 4);
-        }
-        
-        /*this.contextReal.font = "25px Arial";
-        this.contextReal.fillStyle = "red";
-        this.contextReal.textAlign = "center";
-        this.contextReal.fillText("Look I can fill ", coord[0], coord[1]);*/
     }
+    outputText(ctx) {
+        let inputText = $('#textInput').val();
+        contextReal.fillText(inputText, this.textX[0], this.textY[0] + parseInt(this.fontSize));
+        $('#textInput').css({ "display": "none", "transform": "translateY(0) translateX(0)" });
+        $('#textInput').val('');
+        this.textX = [];
+        this.textY = [];
+        console.log('the box is gone no?')
 
+    }
 
 }
