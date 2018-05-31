824b0b2651e543d5f20216c8fecf1d4ebcf69d14
Sam O'Shaughnessy <samoshaughnessy123@hotmail.com>
2018-05-31 12:43:24 +0800

    new changes, added dotted line and stroke width

--- drawing-line.js
+++ drawing-line.js
@@ -1,16 +1,16 @@
 class DrawingLine extends PaintFunction{
     constructor(contextReal){
         super();
-        this.contextReal = contextReal;
+        this.context = contextReal;
     }
     
     onMouseDown(coord,event){
-        this.contextReal.strokeStyle = document.getElementById('colorpickerstroke').value;
-        this.contextReal.lineJoin = "round";
-        this.contextReal.lineWidth = document.getElementById('brushSize').value;
-        this.contextReal.beginPath();
-        this.contextReal.setLineDash([]);
-        this.contextReal.moveTo(coord[0],coord[1]);
+        this.context.strokeStyle = document.getElementById('colorpickerstroke').value;
+        this.context.lineJoin = "round";
+        this.context.lineWidth = document.getElementById('brushSize').value;
+        this.context.beginPath();
+        this.context.setLineDash([]);
+        this.context.moveTo(coord[0],coord[1]);
         this.draw(coord[0],coord[1]);
         console.log('drawing?')
     }
@@ -24,9 +24,9 @@ class DrawingLine extends PaintFunction{
     onMouseEnter(){}
 
     draw(x,y){
-        this.contextReal.lineTo(x,y);
-        this.contextReal.moveTo(x,y);
-        this.contextReal.closePath();
-        this.contextReal.stroke();    
+        this.context.lineTo(x,y);
+        this.context.moveTo(x,y);
+        this.context.closePath();
+        this.context.stroke();    
     }
 } 
