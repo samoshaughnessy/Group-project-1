80bd0e5f46aca36bb373cd63365bdd629cdd5ea8
nerdyckc <chekitsch@protonmail.com>
2018-06-01 15:50:18 +0800

    buggy redo

--- text.js
+++ text.js
@@ -1,35 +1,39 @@
 class Text extends PaintFunction {
-    constructor(contextReal, contextDraft) {
-        super();
-        this.contextReal = contextReal;
-        this.contextDraft = contextDraft;
-        this.fontWeight = 600;
-        this.fontSize = 20;
-        this.fontStyle = "Arial";
-        this.fillStyle = "black";
-        this.textX = [];
-        this.textY = [];
-    }
+  constructor(contextReal, contextDraft) {
+    super();
+    this.contextReal = contextReal;
+    this.contextDraft = contextDraft;
+    this.fontWeight = 600;
+    this.fontSize = 20;
+    this.fontStyle = "Arial";
+    this.fillStyle = "black";
+    this.textX = [];
+    this.textY = [];
+  }
 
-    onMouseDown(coord, event) {
-        this.textX.push(coord[0]);
-        this.textY.push(coord[1]);
-        this.fontStartY = this.textY[0] - this.fontSize;
-        $('#textInput').css({ "display": "block", "transform": "translateY(" + coord[1] + "px) translateX(" + coord[0] + "px)", "font-size": this.fontSize + "px", "color": this.fillStyle, "font-family": this.fontStyle, "font-weight": this.fontWeight, "padding": "0", "border-style": "dotted", "opacity": "0.5" });
-        if ((this.textX.length > 1) && (event.target.id != $('#textInput'))) {
-            this.outputText(this.contextReal);
-            console.log('why no box?')
-        }
+  onMouseDown(coord, event) {
+    this.textX.push(coord[0]);
+    this.textY.push(coord[1]);
+    this.fontStartY = this.textY[0] - this.fontSize;
+    $('#textInput').css({ "display": "block", "transform": "translateY(" + coord[1] + "px) translateX(" + coord[0] + "px)", "font-size": this.fontSize + "px", "color": this.fillStyle, "font-family": this.fontStyle, "font-weight": this.fontWeight, "padding": "0", "border-style": "dotted", "opacity": "0.5" });
+    if ((this.textX.length > 1) && (event.target.id != $('#textInput'))) {
+      this.outputText(this.contextReal);
+      console.log('why no box?')
     }
-    outputText(ctx) {
-        let inputText = $('#textInput').val();
-        contextReal.fillText(inputText, this.textX[0], this.textY[0] + parseInt(this.fontSize));
-        $('#textInput').css({ "display": "none", "transform": "translateY(0) translateX(0)" });
-        $('#textInput').val('');
-        this.textX = [];
-        this.textY = [];
-        console.log('the box is gone no?')
-
-    }
-
+  }
+  outputText(ctx) {
+    let inputText = $('#textInput').val();
+    contextReal.fillText(inputText, this.textX[0], this.textY[0] + parseInt(this.fontSize));
+    $('#textInput').css({ "display": "none", "transform": "translateY(0) translateX(0)" });
+    $('#textInput').val('');
+    this.textX = [];
+    this.textY = [];
+    console.log('the box is gone no?')
+    this.onFinish();
+  }
+  onFinish() {
+    undoObject.states[undoObject.actionCount] = new Image();
+    undoObject.states[undoObject.actionCount].src = canvasReal.toDataURL();
+    undoObject.actionCount++;
+  }
 }
--- undo.js
+++ undo.js
@@ -1,4 +1,34 @@
 var undoObject = {
   states: [],
   actionCount: 0,
+  savePoint: 0,
+  undoAction: function () {
+    if (undoObject.actionCount > 1) {
+      undoObject.actionCount--;
+      undoObject.savePoint = undoObject.actionCount;
+
+      contextReal.fillStyle = "white";
+      contextReal.fillRect(0, 0, canvasReal.width, canvasReal.height);
+
+      contextReal.drawImage(undoObject.states[undoObject.actionCount - 1], 0, 0);
+    }
+  },
+
+  redoAction: function () {
+    if (undoObject.actionCount < undoObject.states.length && undoObject.actionCount == undoObject.savePoint) {
+      undoObject.actionCount++;
+      undoObject.savePoint++;
+      console.log('correct re-do');
+
+      contextReal.fillStyle = "white";
+      contextReal.fillRect(0, 0, canvasReal.width, canvasReal.height);
+
+      contextReal.drawImage(undoObject.states[undoObject.actionCount - 1], 0, 0);
+    } else if (undoObject.actionCount != undoObject.savePoint) {
+      undoObject.states.splice(undoObject.actionCount);
+      console.log('if new action after undos, disgard items beyond savePoint');
+      undoObject.savePoint = undoObject.actionCount;
+    }
+  }
 }
+
